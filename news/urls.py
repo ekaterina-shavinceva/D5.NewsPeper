735bfa7369c23ba8825d53de40c0cf64fac8a306
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-21 10:04:14 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import PostsList, PostDetail, NewsCreate, PostUpdate, PostDelete, NewsSearch
+from .views import (
+   PostsList, PostDetail, NewsCreate, PostUpdate, PostDelete, NewsSearch,
+   subscriptions
+)
 
 urlpatterns = [
    path('', PostsList.as_view(), name='post_list'),
@@ -8,5 +11,6 @@ urlpatterns = [
    path('create/', NewsCreate.as_view(), name='news_create'),
    path('articles/create/', NewsCreate.as_view(), name='news/articles_create'),
    path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
-   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete')
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+   path('subscriptions/', subscriptions, name='subscriptions'),
 ]
