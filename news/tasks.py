53660d137bf7733cf3c333d614854f660f10039c
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-28 18:44:32 +0500

    Update tasks.py

--- news/tasks.py
+++ news/tasks.py
@@ -1,37 +1,12 @@
 from celery import shared_task
-import time
-
-from django.db.models import Exists, OuterRef
-from django.shortcuts import render
-
-from news.models import Subscription, Category
+from django.core.mail import send_mail
 
 
 @shared_task
-def subscriptions(request):
-    if request.method == 'POST':
-        category_id = request.POST.get('category_id')
-        category = Category.objects.get(id=category_id)
-        action = request.POST.get('action')
-
-        if action == 'subscribe':
-            Subscription.objects.create(user=request.user, category=category)
-        elif action == 'unsubscribe':
-            Subscription.objects.filter(
-                user=request.user,
-                category=category,
-            ).delete()
-
-    categories_with_subscriptions = Category.objects.annotate(
-        user_subscribed=Exists(
-            Subscription.objects.filter(
-                user=request.user,
-                category=OuterRef('pk'),
-            )
-        )
-    ).order_by('name')
-    return render(
-        request,
-        'subscriptions.html',
-        {'categories': categories_with_subscriptions},
-    )
+send_mail(
+    'Новые новости на портале'
+    'news/'
+    'EMAIL_HOST_USER'
+    ['subscribers'],
+    fail_silently=False
+)
