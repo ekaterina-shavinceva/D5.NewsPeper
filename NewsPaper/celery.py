9e6d6d62471fbd2d81ec3b778c5db2dd8f164ef3
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-28 18:45:07 +0500

    Update celery.py

--- NewsPaper/celery.py
+++ NewsPaper/celery.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
@@ -7,3 +9,11 @@ app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'mailing': {
+        'tasks': 'mailing',
+        'schedule': crontab(minute='00', hour='8', day_of_week='1'),
+    }
+}
+
