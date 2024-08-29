c8c0714e21b588ec1f0da7c3db2a381cf0c4722f
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-29 10:24:56 +0500

    Update signals.py

--- news/signals.py
+++ news/signals.py
@@ -6,47 +6,51 @@ from django.template.loader import render_to_string
 
 from NewsPaper import settings
 from .models import PostCategory
+from .tasks import send_email_task, weekly_send_mail_task
 
-def send_notifications(preview, pk, title, subscribers):
-    html_content = render_to_string(
-        'post_created_email.html',
-        {
-            'text': preview,
-            'link': f'{settings.SITE_URL}/news/{pk}'
-        }
-    )
 
-    msg = EmailMultiAlternatives(
-        subject=title,
-        body='',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=subscribers,
-    )
-
-    msg.attach_alternative(html_content, 'text/html')
-    msg.send()
+# def send_notifications(preview, pk, title, subscribers):
+#     html_content = render_to_string(
+#         'post_created_email.html',
+#         {
+#             'text': preview,
+#             'link': f'{settings.SITE_URL}/news/{pk}'
+#         }
+#     )
+#
+#     msg = EmailMultiAlternatives(
+#         subject=title,
+#         body='',
+#         from_email=settings.DEFAULT_FROM_EMAIL,
+#         to=subscribers,
+#     )
+#
+#     msg.attach_alternative(html_content, 'text/html')
+#     msg.send()
 
 @receiver(m2m_changed, sender=PostCategory)
 def post_created(sender, instance, **kwargs):
     if not kwargs['action'] == 'post_add':
         return
 
-    emails = User.objects.filter(
-        subscriptions__category__in=instance.category.all()
-    ).values_list('email', flat=True)
+    # emails = User.objects.filter(
+    #     subscriptions__category__in=instance.category.all()
+    # ).values_list('email', flat=True)
+    #
+    # subject = f'Новый пост в категории {",".join([cat.name for cat in instance.category.all()])}'
+    #
+    # text_content = (
+    #     f'Заголовок поста: {instance.title}\n'
+    #     f'Ссылка на пост: http://127.0.0.1:8000{instance.get_absolute_url()}'
+    # )
+    # html_content = (
+    #     f'Заголовок поста: {instance.title}<br>'
+    #     f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
+    #     f'Ссылка на пост</a>'
+    # )
+    # for email in emails:
+    #     msg = EmailMultiAlternatives(subject, text_content, None, [email])
+    #     msg.attach_alternative(html_content, 'text/html')
+    #     msg.send()
 
-    subject = f'Новый пост в категории {",".join([cat.name for cat in instance.category.all()])}'
-
-    text_content = (
-        f'Заголовок поста: {instance.title}\n'
-        f'Ссылка на пост: http://127.0.0.1:8000{instance.get_absolute_url()}'
-    )
-    html_content = (
-        f'Заголовок поста: {instance.title}<br>'
-        f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
-        f'Ссылка на пост</a>'
-    )
-    for email in emails:
-        msg = EmailMultiAlternatives(subject, text_content, None, [email])
-        msg.attach_alternative(html_content, 'text/html')
-        msg.send()
+    send_email_task.delay(instance.id)
