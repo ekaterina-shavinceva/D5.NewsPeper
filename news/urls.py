3782ca36be97840b0a57972606a79c6d98b0d1e3
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-09-03 10:40:41 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from .views import (
    PostsList, PostDetail, NewsCreate, PostUpdate, PostDelete, NewsSearch,
-   subscriptions,
-   # CategoryListView
+   subscriptions
 )
+from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
-   path('', PostsList.as_view(), name='post_list'),
-   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('', cache_page(60)(PostsList.as_view()), name='post_list'),
+   path('<int:pk>', cache_page(300)(PostDetail.as_view()), name='post_detail'),
    path('search/', NewsSearch.as_view(), name='post_search'),
    path('create/', NewsCreate.as_view(), name='news_create'),
    path('articles/create/', NewsCreate.as_view(), name='news/articles_create'),
    path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
    path('subscriptions/', subscriptions, name='subscriptions'),
+
 ]
